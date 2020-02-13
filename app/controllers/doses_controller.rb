471427a66f93516ce14480da13a0f2f28ea5016a
Augustin Dortu <augustin.dortu@edhec.com>
2020-02-13 22:46:32 +0100

    jour 1

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,20 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      redirect_to dose_path(@dose)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:name)
+  end
+end
